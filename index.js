5a40705f09c9a697bfd36519d7f7b08bce333d41
christina kaliora <develop@pirron.one>
2019-11-13 00:38:45 +0100

    passed all tests

--- index.js
+++ index.js
@@ -158,8 +158,14 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  const arr = [];
+  list.forEach(function(item){
+    if (arr.indexOf(item) === -1){
+      arr.push(item);
+    }
+  });
+  return callback(arr);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
@@ -200,8 +206,10 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map((item) => {
+    return item.first_name.toUpperCase();
+  });
 }
 
 /**
@@ -217,8 +225,10 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter((item) => {
+    return item.shirt_size === tShirtSize;
+  });
 }
 
 /**
@@ -231,8 +241,10 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  return runners.reduce(function(acc, item){
+    return acc + item.donation;
+  },0);
 }
 
 /////////////// CLOSURES ///////////////
@@ -253,11 +265,10 @@ function tallyUpDonations(/* CODE HERE */) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = 0;
+  return function(){
+    return count++;
   }
-  // BROKEN CODE ENDS
 }
 
 /**
@@ -280,8 +291,14 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(max) {
+  let count = 0;
+  return function(){
+    if (count > max){
+      count = 0;  
+    }
+    return count++;
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
